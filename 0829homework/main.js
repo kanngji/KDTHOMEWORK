af4f2ca7c62dee000725469aefa5d9f8699b9bd7
kanngji <50470748+kanngji@users.noreply.github.com>
2022-08-29 01:04:49 +0900

    fb login api

---
+++ 0829homework/main.js
@@ -0,0 +1,15 @@
+// app id
+// window.fbAsyncInit = function () {
+//   FB.init({
+//     appId: "",
+//     cookie: true, // Enable cookies to allow the server to access the session.
+//     xfbml: true, // Parse social plugins on this webpage.
+//     version: "v14.0", // Use this Graph API version for this call.
+//   });
+//   //로그인 되어있는지 아닌지의 상태를 가져온다
+//   var callback = function (response) {
+//     console.log(response);
+//     // statusChangeCallback(response);
+//   };
+//   FB.getLoginStatus(callback);
+// };
